c4ee769c4ce9656b304515e1442f6c7556c79dfb
Simon Lentz <simonemlentz@gmail.com>
2025-10-01 16:49:22 +0200

    add assertion helper to tests

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,37 @@
+defmodule TestHelper do
+  require ExUnit.Assertions
+
+  defmacro __using__(_) do
+    quote do
+      import unquote(__MODULE__)
+    end
+  end
+
+  defmacro assert_eventually(opts \\ [], do: block) do
+    timeout = opts[:timeout] || 1000
+    step = opts[:step] || 100
+
+    quote do
+      assert_eventually(
+        fn -> unquote(block) end,
+        unquote(timeout),
+        unquote(step)
+      )
+    end
+  end
+
+  def assert_eventually(_f, timeout, _step) when timeout <= 0,
+    do: ExUnit.Assertions.assert(false, "Timed out")
+
+  def assert_eventually(f, timeout, step) do
+    try do
+      f.()
+    rescue
+      ExUnit.AssertionError ->
+        Process.sleep(step)
+        assert_eventually(f, timeout - step, step)
+    end
+  end
+end
+
 ExUnit.start()
