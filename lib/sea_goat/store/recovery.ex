314671db5c2013fa91a580ae665227d0d1ced346
Simon Lentz <simonemlentz@gmail.com>
2025-10-02 22:01:09 +0200

    Refactor Store.Recovery

--- lib/sea_goat/store/recovery.ex
+++ lib/sea_goat/store/recovery.ex
@@ -9,99 +9,119 @@ defmodule SeaGoat.Store.Recovery do
   require SeaGoat.Compactor
 
   def recover_state(dir, wal, compactor) do
-    recovered_state = %{
-      max_file_count: 0,
-      max_wal_count: 0,
-      files: %{},
-      recovered_writes: [],
-      recovered_compacting_files: %{}
-    }
+    dir
+    |> get_files()
+    |> parse_files(wal)
+    |> recover(compactor)
+  end
 
+  defp get_files(dir) do
     dir
     |> File.ls!()
     |> Enum.map(&Path.join(dir, &1))
     |> Enum.filter(&FileManager.valid_db_file?/1)
-    |> Enum.flat_map(&parse_file(&1, wal))
-    |> Enum.sort_by(& &1.file_count)
-    |> Enum.reduce(recovered_state, &process_file(&1, &2, compactor))
+    |> Enum.sort_by(&FileManager.file_count_from_path/1)
   end
 
-  defp parse_file(file, wal) do
-    case wal_or_ss_table(file, wal) do
-      {:logs, logs} ->
-        file_count = FileManager.file_count_from_path(file)
-        [%{type: :log, logs: logs, file: file, file_count: file_count}]
+  defp parse_files(files, wal) do
+    Enum.reduce(files, %{wal_files: [], ss_table_files: []}, fn file, acc ->
+      case categorize_file(file, wal) do
+        {:wal, logs} ->
+          file_count = FileManager.file_count_from_path(file)
+          wal_entry = {file, file_count, logs}
+          %{acc | wal_files: [wal_entry | acc.wal_files]}
 
-      {:ss_table, bloom_filter, level} ->
-        file_count = FileManager.file_count_from_path(file)
+        {:ss_table, bloom_filter, level} ->
+          file_count = FileManager.file_count_from_path(file)
+          ss_table_entry = {file, file_count, bloom_filter, level}
+          %{acc | ss_table_files: [ss_table_entry | acc.ss_table_files]}
 
-        [
-          %{
-            type: :ss_table,
-            bloom_filter: bloom_filter,
-            level: level,
-            file: file,
-            file_count: file_count
-          }
-        ]
-
-      _ ->
-        []
-    end
+        _ ->
+          acc
+      end
+    end)
   end
 
-  defp process_file(%{type: :log} = file, recovered_state, _compactor) do
-    case file.logs do
-      [SeaGoat.Writer.writer_tag() | _logs] ->
-        recovered_writes = [{file.file, file.logs} | recovered_state.recovered_writes]
-
+  defp recover(%{wal_files: wal_files, ss_table_files: ss_table_files}, compactor) do
+    wal_state =
+      Enum.reduce(
+        wal_files,
         %{
-          recovered_state
-          | recovered_writes: recovered_writes,
-            max_file_count: file.file_count,
-            max_wal_count: file.file_count
-        }
+          max_file_count: 0,
+          max_wal_count: 0,
+          recovered_writes: [],
+          recovered_compacting_files: %{}
+        },
+        &process_wal_file/2
+      )
 
-      [{SeaGoat.Compactor.compactor_tag(), files} | logs] ->
-        recovered_compacting_files = Map.put(recovered_state.recovered_compacting_files, files, file.file)
-        :ok = run_logs(logs)
-        %{recovered_state | max_file_count: file.file_count, recovered_compacting_files: recovered_compacting_files}
+    ss_table_state =
+      Enum.reduce(
+        ss_table_files,
+        %{max_file_count: 0, files: %{}},
+        &process_ss_table_file(&1, &2, compactor)
+      )
 
-      logs ->
-        :ok = run_logs(logs)
-        %{recovered_state | max_file_count: file.file_count}
-    end
+    %{
+      max_file_count: max(wal_state.max_file_count, ss_table_state.max_file_count),
+      max_wal_count: wal_state.max_wal_count,
+      files: ss_table_state.files,
+      recovered_writes: wal_state.recovered_writes,
+      recovered_compacting_files: wal_state.recovered_compacting_files
+    }
   end
 
-  defp process_file(%{type: :ss_table} = file, recovered_state, compactor) do
-    files = Files.insert(recovered_state.files, file.file, file.bloom_filter)
-    :ok = Compactor.put(compactor, file.level, file.file_count, file.file)
-    %{recovered_state | files: files, max_file_count: file.file_count}
+  defp process_wal_file({file, file_count, [SeaGoat.Writer.writer_tag() | logs]}, state) do
+    recovered_writes = [{file, logs} | state.recovered_writes]
+
+    %{
+      state
+      | recovered_writes: recovered_writes,
+        max_file_count: file_count,
+        max_wal_count: file_count
+    }
   end
 
-  defp run_logs([]), do: :ok
-
-  defp run_logs([{:del, files} | logs]) do
-    with :ok <- SSTables.delete(files) do
-      run_logs(logs)
-    end
+  defp process_wal_file(
+         {file, file_count, [{SeaGoat.Compactor.compactor_tag(), files} | logs]},
+         state
+       ) do
+    recovered_compacting_files = Map.put(state.recovered_compacting_files, files, file)
+    :ok = execute_logs(logs)
+    %{state | max_file_count: file_count, recovered_compacting_files: recovered_compacting_files}
   end
 
-  defp run_logs([_ | logs]), do: run_logs(logs)
+  defp process_wal_file({_file, file_count, logs}, state) do
+    :ok = execute_logs(logs)
+    %{state | max_file_count: file_count}
+  end
 
-  defp wal_or_ss_table(file, wal) do
+  defp process_ss_table_file({file, file_count, bloom_filter, level}, state, compactor) do
+    files = Files.insert(state.files, file, bloom_filter)
+    :ok = Compactor.put(compactor, level, file_count, file)
+    %{state | files: files, max_file_count: file_count}
+  end
+
+  defp categorize_file(file, wal) do
     case WAL.get_logs(wal, file) do
       {:ok, logs} ->
-        {:logs, logs}
+        {:wal, logs}
 
       _ ->
         case SSTables.fetch_ss_table_info(file) do
-          {:ok, bloom_filter, level} ->
-            {:ss_table, bloom_filter, level}
-
-          _ ->
-            {:error, :not_wal_or_db}
+          {:ok, bloom_filter, level} -> {:ss_table, bloom_filter, level}
+          _ -> :no_file
         end
     end
   end
+
+  defp execute_logs([]), do: :ok
+
+  defp execute_logs([{:del, files} | logs]) do
+    with :ok <- SSTables.delete(files) do
+      execute_logs(logs)
+    end
+  end
+
+  defp execute_logs([_ | logs]), do: execute_logs(logs)
 end
