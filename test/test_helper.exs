1d28313b64c4cc26da5af48cd5d8bb30a610cbb9
Simon Lentz <simonemlentz@gmail.com>
2025-10-02 15:22:30 +0200

    Add helper macros for setting up the DB supervisor

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -7,6 +7,54 @@ defmodule TestHelper do
     end
   end
 
+  defmacro setup_db(opts \\ []) do
+    opts = Macro.expand(opts, __CALLER__)
+
+    quote do
+      setup c do
+        start_db(c.tmp_dir, unquote(opts))
+      end
+    end
+  end
+
+  defmacro start_db(dir, opts \\ []) do
+    id = opts[:id]
+    name = opts[:name]
+    level_limit = opts[:level_limit]
+    sync_interval = opts[:sync_interval]
+    wal_name = opts[:wal_name]
+
+    quote do
+      db =
+        start_supervised!(
+          {SeaGoat,
+           name: unquote(name),
+           dir: unquote(dir),
+           level_limit: unquote(level_limit),
+           wal_name: unquote(wal_name),
+           sync_interval: unquote(sync_interval)},
+          id: unquote(id)
+        )
+
+      [
+        {SeaGoat.Writer, writer, _, _},
+        {SeaGoat.Store, store, _, _},
+        {SeaGoat.Compactor, compactor, _, _},
+        {SeaGoat.WAL, wal, _, _},
+        {SeaGoat.RWLocks, rw_locks, _, _}
+      ] = Supervisor.which_children(db)
+
+      %{
+        db_id: unquote(id),
+        writer: writer,
+        store: store,
+        compactor: compactor,
+        wal: wal,
+        rw_locks: rw_locks
+      }
+    end
+  end
+
   defmacro assert_eventually(opts \\ [], do: block) do
     timeout = opts[:timeout] || 1000
     step = opts[:step] || 100
