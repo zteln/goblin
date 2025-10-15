b5e418ee24e473b96561ff60e9e413a1a1e308ca
Simon Lentz <simonemlentz@gmail.com>
2025-10-15 20:25:15 +0200

    Remove :tools extra_application

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule SeaGoat.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :wx, :observer, :tools]
+      extra_applications: [:logger, :wx, :observer]
     ]
   end
 
