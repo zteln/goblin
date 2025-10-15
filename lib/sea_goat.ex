305f88ccd174779f146c9b1e67ab26ead3bf281a
Simon Lentz <simonemlentz@gmail.com>
2025-10-15 20:22:35 +0200

    Update types, merge keyword args to children

--- lib/sea_goat.ex
+++ lib/sea_goat.ex
@@ -1,6 +1,8 @@
 defmodule SeaGoat do
   use Supervisor
 
+  @type db_key_limit :: non_neg_integer()
+  @type db_level_limit :: non_neg_integer()
   @type db_key :: term()
   @type db_value :: term() | nil
   @type db_level :: non_neg_integer()
@@ -8,11 +10,18 @@ defmodule SeaGoat do
   @type db_file :: String.t()
   @type db_server :: GenServer.server()
 
+  @default_key_limit 50_000
+  @default_level_limit 128 * 1024 * 1024
+
   @spec put(db_server(), db_key(), db_value()) :: :ok
   def put(db, key, value) do
     writer = name(db, :writer)
     SeaGoat.Writer.put(writer, key, value)
   end
+
+  # def put_multi do
+  #
+  # end
 
   @spec remove(db_server(), db_key()) :: :ok
   def remove(db, key) do
@@ -20,58 +29,88 @@ defmodule SeaGoat do
     SeaGoat.Writer.remove(writer, key)
   end
 
+  # def remove_multi do
+  #
+  # end
+
   @spec get(db_server(), db_key()) :: db_value() | nil
-  def get(db, key) do
+  def get(db, key, default \\ nil) do
     writer = name(db, :writer)
     store = name(db, :store)
-    SeaGoat.Reader.get(writer, store, key)
+
+    case SeaGoat.Reader.get(writer, store, key) do
+      :not_found -> default
+      {_seq, value} -> value
+    end
   end
+
+  # def get_multi do
+  #
+  # end
+
+  # def select do
+  #
+  # end
+
+  # def is_compacting do
+  #
+  # end
+  #
+  # def is_flushing do
+  #
+  # end
 
   @spec start_link(keyword()) :: Supervisor.on_start()
   def start_link(opts) do
-    opts[:dir] || raise "no dir provided."
+    opts[:db_dir] || raise "no db_dir provided."
     Supervisor.start_link(__MODULE__, opts, name: opts[:name] || __MODULE__)
   end
 
   @impl true
-  def init(opts) do
-    rw_locks_name = name(opts[:name], :rw_locks)
-    manifest_name = name(opts[:name], :manifest)
-    wal_name = name(opts[:name], :wal)
-    writer_name = name(opts[:name], :writer)
-    compactor_name = name(opts[:name], :compactor)
-    store_name = name(opts[:name], :store)
+  def init(args) do
+    db_dir = args[:db_dir]
+    key_limit = args[:key_limit] || @default_key_limit
+    level_limit = args[:level_limit] || @default_level_limit
+    rw_locks_name = name(args[:name], :rw_locks)
+    manifest_name = name(args[:name], :manifest)
+    wal_name = name(args[:name], :wal)
+    writer_name = name(args[:name], :writer)
+    compactor_name = name(args[:name], :compactor)
+    store_name = name(args[:name], :store)
 
     children = [
-      {SeaGoat.RWLocks, name: rw_locks_name},
-      {SeaGoat.Manifest, name: manifest_name, dir: opts[:dir]},
-      {SeaGoat.WAL,
-       name: wal_name,
-       wal_name: opts[:wal_name],
-       sync_interval: opts[:sync_interval],
-       dir: opts[:dir]},
-      {SeaGoat.Recovery, dir: opts[:dir], manifest: manifest_name},
+      {SeaGoat.RWLocks, Keyword.merge(args, name: rw_locks_name)},
+      {SeaGoat.Manifest, Keyword.merge(args, name: manifest_name, db_dir: db_dir)},
+      {SeaGoat.WAL, Keyword.merge(args, name: wal_name, db_dir: db_dir)},
+      {SeaGoat.Recovery, Keyword.merge(args, dir: db_dir, manifest: manifest_name)},
       {SeaGoat.Compactor,
-       name: compactor_name,
-       manifest: manifest_name,
-       store: store_name,
-       rw_locks: rw_locks_name,
-       level_limit: opts[:level_limit]},
-      {
-        SeaGoat.Store,
-        name: store_name,
-        dir: opts[:dir],
-        writer: writer_name,
-        manifest: manifest_name,
-        rw_locks: rw_locks_name,
-        compactor: compactor_name
-      },
+       Keyword.merge(args,
+         name: compactor_name,
+         key_limit: key_limit,
+         level_limit: level_limit,
+         manifest: manifest_name,
+         store: store_name,
+         rw_locks: rw_locks_name,
+         level_limit: level_limit
+       )},
+      {SeaGoat.Store,
+       Keyword.merge(args,
+         name: store_name,
+         dir: db_dir,
+         writer: writer_name,
+         manifest: manifest_name,
+         rw_locks: rw_locks_name,
+         compactor: compactor_name
+       )},
       {SeaGoat.Writer,
-       name: writer_name,
-       wal: wal_name,
-       store: store_name,
-       manifest: manifest_name,
-       limit: opts[:limit]}
+       Keyword.merge(args,
+         name: writer_name,
+         key_limit: key_limit,
+         wal: wal_name,
+         store: store_name,
+         manifest: manifest_name,
+         limit: key_limit
+       )}
     ]
 
     Supervisor.init(children, strategy: :one_for_all)
