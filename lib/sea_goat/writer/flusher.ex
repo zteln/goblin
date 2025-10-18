ce15a8fa6fb143b35e8d714f06831c0819f0f23a
Simon Lentz <simonemlentz@gmail.com>
2025-10-18 14:48:50 +0200

    Update Flusher with new Manifest.log_flush, add test for flusher

--- lib/sea_goat/writer/flusher.ex
+++ lib/sea_goat/writer/flusher.ex
@@ -16,11 +16,12 @@ defmodule SeaGoat.Writer.Flusher do
   def flush(mem_table, rotated_wal, {store, wal, manifest}) do
     file = Store.new_file(store)
     tmp_file = Store.tmp_file(file)
+    stream = flush_stream(mem_table)
 
     with {:ok, bloom_filter, priority, size, key_range} <-
-           SSTables.write(tmp_file, @flush_level, flush_stream(mem_table)),
+           SSTables.write(tmp_file, @flush_level, stream),
          :ok <- SSTables.switch(tmp_file, file),
-         :ok <- Manifest.log_file_added(manifest, file),
+         :ok <- Manifest.log_flush(manifest, file, rotated_wal),
          :ok <- WAL.clean(wal, rotated_wal),
          :ok <-
            Store.put(store, file, @flush_level, {bloom_filter, priority, size, key_range}) do
@@ -30,19 +31,20 @@ defmodule SeaGoat.Writer.Flusher do
 
   defp flush_stream(mem_table) do
     Stream.resource(
-      fn ->
-        mem_table
-        |> Enum.sort()
-        |> Enum.map(fn {key, {seq, value}} -> {seq, key, value} end)
-      end,
-      fn
-        [] ->
-          {:halt, :ok}
-
-        [next | data] ->
-          {[next], data}
-      end,
-      fn _ -> :ok end
+      fn -> init_flush_data(mem_table) end,
+      &iter_flush_data/1,
+      &after_iter/1
     )
   end
+
+  defp init_flush_data(mem_table) do
+    mem_table
+    |> Enum.sort()
+    |> Enum.map(fn {key, {seq, value}} -> {seq, key, value} end)
+  end
+
+  defp iter_flush_data([]), do: {:halt, :ok}
+  defp iter_flush_data([next | data]), do: {[next], data}
+
+  defp after_iter(_), do: :ok
 end
---
+++ test/sea_goat/writer/flusher_test.exs
@@ -0,0 +1,35 @@
+defmodule SeaGoat.Writer.FlusherTest do
+  use ExUnit.Case, async: true
+  use TestHelper
+  alias SeaGoat.Writer.Flusher
+  alias SeaGoat.Writer.MemTable
+  alias SeaGoat.SSTables
+
+  @moduletag :tmp_dir
+  @db_opts [
+    id: :flusher_test,
+    name: :flusher_test,
+    wal_name: :flusher_test_wal_log,
+    manifest_name: :flusher_test_log_name
+  ]
+
+  setup_db(@db_opts)
+
+  test "flush/3 writes an SST file on disk", c do
+    rotated_wal = Path.join(c.tmp_dir, "rot_wal")
+    File.touch(rotated_wal)
+
+    mem_table =
+      MemTable.new()
+      |> MemTable.upsert(0, :k1, :v1)
+      |> MemTable.upsert(1, :k2, :v2)
+
+    assert :flushed == Flusher.flush(mem_table, rotated_wal, {c.store, c.wal, c.manifest})
+
+    refute File.exists?(rotated_wal)
+
+    assert %{ss_tables: [%{file: flushed_file}]} = :sys.get_state(c.store)
+
+    assert [{0, :k1, :v1}, {1, :k2, :v2}] == SSTables.stream(flushed_file) |> Enum.to_list()
+  end
+end
