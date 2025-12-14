06be16c9db425dd9494516bec69f2bcce69aa750
Simon Lentz <simonemlentz@gmail.com>
2025-12-14 09:30:07 +0100

    Rewrite Writer tests

--- test/goblin/writer_test.exs
+++ test/goblin/writer_test.exs
@@ -1,4 +1,19 @@
 defmodule Goblin.WriterTest do
+  @moduledoc """
+  Important qualities to test:
+
+  - Non-trivial API:
+    - `get/3`
+    - `get_multi/2`
+    - `iterators/3`
+    - `transation/2`
+  - Properties:
+    - Flushes to DiskTable when exceeding mem limit
+    - Can read data during flush
+    - Recovers state from WAL on start
+    - Transactions are queued
+    - Transaction processes are cleaned up on failure
+  """
   use ExUnit.Case, async: true
   use TestHelper
   alias Goblin.Writer
@@ -13,143 +28,155 @@ defmodule Goblin.WriterTest do
   @moduletag :tmp_dir
   setup_db()
 
-  describe "get/3, get_multi/3" do
-    test "returns :not_found for non-existing key" do
+  describe "API" do
+    test "get/3 returns :not_found for non-existing key" do
       assert :not_found == Writer.get(@table, :k)
     end
 
-    test "returns corresponding value", c do
-      put(c.writer, :k, :v)
+    test "get/3 returns value associated with key", c do
+      Goblin.put(c.db, :k, :v)
       assert {:value, 0, :v} == Writer.get(@table, :k)
     end
 
-    test "returns tuple of keys found and keys not found", c do
-      put(c.writer, :k1, :v1)
-      put(c.writer, :k2, :v2)
+    test "get/3 returns tombstone value if key is removed", c do
+      Goblin.put(c.db, :k, :v)
+      assert {:value, 0, :v} == Writer.get(@table, :k)
+      Goblin.remove(c.db, :k)
+      assert {:value, 1, :"$goblin_tombstone"} == Writer.get(@table, :k)
+    end
+
+    test "get_multi/2 returns tuple of keys found and keys not found", c do
+      Goblin.put(c.db, :k1, :v1)
+      Goblin.put(c.db, :k2, :v2)
 
       assert {[{:k2, 1, :v2}, {:k1, 0, :v1}], []} ==
                Writer.get_multi(@table, [:k1, :k2])
 
       assert {[], [:k4, :k3]} == Writer.get_multi(@table, [:k3, :k4])
-      assert {[{:k1, 0, :v1}], [:k4, :k3]} == Writer.get_multi(@table, [:k1, :k3, :k4])
+
+      assert {[{:k1, 0, :v1}], [:k4, :k3]} ==
+               Writer.get_multi(@table, [:k1, :k3, :k4])
     end
 
-    test "returns :$goblin_tombstone as value if key is removed", c do
-      remove(c.writer, :k)
-      assert {:value, 0, :"$goblin_tombstone"} == Writer.get(@table, :k)
-    end
-
-    @tag db_opts: [task_mod: FakeTask, mem_limit: 1]
-    test "can read during flush", c do
-      data =
-        for n <- 1..10 do
-          {:"k#{n}", :"v#{n}"}
-        end
-
-      put_multi(c.writer, data)
-
-      assert %{flushing: {_, _, _}} = :sys.get_state(c.writer)
-
+    test "iterators/3 returns iterators iterating through relevant data", c do
       for n <- 1..10 do
-        assert {:value, n - 1, :"v#{n}"} == Writer.get(@table, :"k#{n}")
+        Goblin.put(c.db, n, "v-#{n}")
       end
 
-      keys = for(n <- 1..10, do: :"k#{n}")
+      assert {data, next, close} = Writer.iterators(@table, 1, 10)
 
-      assert {for(n <- 10..1//-1, do: {:"k#{n}", n - 1, :"v#{n}"}), []} ==
-               Writer.get_multi(@table, keys)
-    end
-  end
+      data =
+        for n <- 1..10, reduce: data do
+          acc ->
+            key = n
+            seq = n - 1
+            value = "v-#{n}"
+            assert {{^key, ^seq, ^value}, acc} = next.(acc)
+            acc
+        end
 
-  describe "iterators/3" do
-    test "returns empty range if MemTable is empty" do
-      assert {[], _, _} = Writer.iterators(@table, nil, nil)
-    end
-
-    test "returns iterator over MemTable data", c do
-      put(c.writer, :k1, :v1)
-      put(c.writer, :k2, :v2)
-
-      assert {[{:k1, 0, :v1}, {:k2, 1, :v2}], _, _} =
-               Writer.iterators(@table, nil, nil)
+      assert :ok == close.(data)
     end
 
-    test "returns iterator over MemTable subset of data", c do
-      put(c.writer, :k1, :v1)
-      put(c.writer, :k2, :v2)
-      put(c.writer, :k3, :v3)
+    test "transaction/2 updates mem table on commit", c do
+      assert nil == Goblin.get(c.db, :k)
 
-      assert {[{:k2, 1, :v2}], _, _} = Writer.iterators(@table, :k2, :k2)
-      assert {[{:k1, 0, :v1}, {:k2, 1, :v2}], _, _} = Writer.iterators(@table, nil, :k2)
-      assert {[{:k2, 1, :v2}, {:k3, 2, :v3}], _, _} = Writer.iterators(@table, :k2, nil)
-    end
-
-    test "iterates over provided range", c do
-      put(c.writer, :k1, :v1)
-      put(c.writer, :k2, :v2)
-      put(c.writer, :k3, :v3)
-
-      assert {[{:k1, 0, :v1}, {:k2, 1, :v2}, {:k3, 2, :v3}] = state, next, close} =
-               Writer.iterators(@table, nil, nil)
-
-      assert {{:k1, 0, :v1}, state} = next.(state)
-      assert {{:k2, 1, :v2}, state} = next.(state)
-      assert {{:k3, 2, :v3}, state} = next.(state)
-      assert :ok == next.(state)
-      assert :ok == close.(state)
-    end
-  end
-
-  describe "transaction/2" do
-    test "commits writes to MemTable", c do
       assert :ok ==
                Writer.transaction(c.writer, fn tx ->
                  tx = Goblin.Tx.put(tx, :k, :v)
                  {:commit, tx, :ok}
                end)
 
-      assert {:value, 0, :v} == Writer.get(@table, :k)
+      assert :v == Goblin.get(c.db, :k)
     end
 
-    test "if cancelled does not commit to MemTable", c do
+    test "transaction/2 does not update mem table when cancelled", c do
+      assert nil == Goblin.get(c.db, :k)
+
       assert :ok ==
                Writer.transaction(c.writer, fn tx ->
-                 Goblin.Tx.put(tx, :k, :v)
+                 _tx = Goblin.Tx.put(tx, :k, :v)
                  :cancel
                end)
 
-      assert :not_found == Writer.get(@table, :k)
+      assert nil == Goblin.get(c.db, :k)
     end
 
-    test "writer state cleans writer if writer pid exits mid-transaction", c do
-      parent = self()
+    test "transaction/2 does not allow nested transactions", c do
+      assert :ok ==
+               Writer.transaction(c.writer, fn tx ->
+                 assert {:error, :already_in_tx} ==
+                          Writer.transaction(c.writer, fn tx ->
+                            {:commit, tx, :ok}
+                          end)
 
-      pid =
-        spawn(fn ->
-          Writer.transaction(c.writer, fn _tx ->
-            send(parent, :ready)
+                 {:commit, tx, :ok}
+               end)
+    end
 
-            receive do
-              :cont -> :ok
-            end
+    test "latest_commit_sequence/1 returns default seq (0) if no commits exist" do
+      assert 0 == Writer.latest_commit_sequence(@table)
+    end
 
-            exit(:normal)
-          end)
-        end)
+    test "latest_commit_sequence/1 returns latest commit sequence", c do
+      Goblin.put(c.db, :k, :v)
 
-      receive do
-        :ready -> :ok
-      end
+      assert 1 == Writer.latest_commit_sequence(@table)
 
-      assert %{writer: {^pid, _ref}} = :sys.get_state(c.writer)
-      send(pid, :cont)
+      Goblin.put(c.db, :k, :v)
+      Goblin.put(c.db, :k, :v)
+      Goblin.put(c.db, :k, :v)
+
+      assert 4 == Writer.latest_commit_sequence(@table)
+    end
+
+    @tag db_opts: [mem_limit: 2 * 1024, task_mod: FakeTask]
+    test "is_flushing/1 returns boolean indicating whether writer is flushing or not", c do
+      refute Writer.is_flushing(c.writer)
+      trigger_flush(c.db)
+      assert Writer.is_flushing(c.writer)
+    end
+  end
+
+  describe "property" do
+    @tag db_opts: [mem_limit: 2 * 1024]
+    test "flushes mem table to disk table(s) when exceeding memory limit", c do
+      data = trigger_flush(c.db)
 
       assert_eventually do
-        assert %{writer: nil} = :sys.get_state(c.writer)
+        files = File.ls!(c.tmp_dir)
+        assert [sst_file] = Enum.filter(files, &String.match?(&1, ~r/^\d+\.goblin$/))
+
+        assert {:ok, %{key_range: {min, max}}} =
+                 Goblin.DiskTable.fetch_sst(Path.join(c.tmp_dir, sst_file))
+
+        assert {^min, _} = Enum.min_by(data, &elem(&1, 0))
+        assert {^max, _} = Enum.max_by(data, &elem(&1, 0))
       end
     end
 
-    test "writers are queued", c do
+    @tag db_opts: [mem_limit: 2 * 1024, task_mod: FakeTask]
+    test "can read during a flush", c do
+      data = trigger_flush(c.db)
+
+      assert Goblin.is_flushing(c.db)
+
+      assert {Enum.map(data, fn {k, v} -> {k, k - 1, v} end) |> Enum.reverse(), []} ==
+               Writer.get_multi(@table, Enum.map(data, fn {k, _v} -> k end))
+
+      assert Goblin.is_flushing(c.db)
+    end
+
+    test "recovers state from WAL on restart", c do
+      Goblin.put(c.db, :k, :v)
+
+      stop_db(__MODULE__)
+      start_db(c.tmp_dir, name: __MODULE__)
+
+      assert {:value, 0, :v} == Writer.get(@table, :k)
+    end
+
+    test "transactions are queued", c do
       parent = self()
 
       pid1 =
@@ -190,52 +217,47 @@ defmodule Goblin.WriterTest do
       end
     end
 
-    # @tag db_opts: [mem_limit: 1024 * 1024]
-    # test "flushes to disk if MemTable exceeds size limit", c do
-    #   # TODO: Fix this test
-    #   data =
-    #     for n <- 1..10 do
-    #       {n, :binary.copy(<<"#{n}">>, 1024 * 1024)}
-    #     end
-    #
-    #   assert :ok == put_multi(c.writer, data)
-    #
-    #   assert_eventually do
-    #     files = File.ls!(c.tmp_dir)
-    #     assert [sst_file] = Enum.filter(files, &String.match?(&1, ~r/^\d+\.goblin$/))
-    #
-    #     assert {:ok, %{seq_range: {0, 9}, key_range: {:k1, :k9}}} =
-    #              Goblin.DiskTable.fetch_sst(Path.join(c.tmp_dir, sst_file))
-    #   end
-    #
-    #   for n <- 1..10 do
-    #     assert :not_found == Writer.get(@table, :"k#{n}")
-    #   end
-    # end
-  end
+    test "failing transaction holder gets cleaned", c do
+      parent = self()
 
-  defp remove(writer, k) do
-    Writer.transaction(writer, fn tx ->
-      tx = Goblin.Tx.remove(tx, k)
-      {:commit, tx, :ok}
-    end)
-  end
+      pid =
+        spawn(fn ->
+          Writer.transaction(c.writer, fn _tx ->
+            send(parent, :ready)
 
-  defp put(writer, k, v) do
-    Writer.transaction(writer, fn tx ->
-      tx = Goblin.Tx.put(tx, k, v)
-      {:commit, tx, :ok}
-    end)
-  end
+            receive do
+              :cont -> :ok
+            end
 
-  defp put_multi(writer, pairs) do
-    Goblin.Writer.transaction(writer, fn tx ->
-      tx =
-        Enum.reduce(pairs, tx, fn {k, v}, acc ->
-          Goblin.Tx.put(acc, k, v)
+            exit(:normal)
+          end)
         end)
 
-      {:commit, tx, :ok}
+      receive do
+        :ready -> :ok
+      end
+
+      assert %{writer: {^pid, _ref}} = :sys.get_state(c.writer)
+      send(pid, :cont)
+
+      assert_eventually do
+        assert %{writer: nil} = :sys.get_state(c.writer)
+      end
+    end
+  end
+
+  defp trigger_flush(db) do
+    Stream.iterate(1, &(&1 + 1))
+    |> Stream.map(&{&1, "v-#{&1}"})
+    |> Stream.chunk_every(1000)
+    |> Stream.transform(nil, fn chunk, acc ->
+      if Goblin.is_flushing(db) do
+        {:halt, acc}
+      else
+        Goblin.put_multi(db, chunk)
+        {chunk, acc}
+      end
     end)
+    |> Enum.to_list()
   end
 end
