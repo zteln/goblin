f9720109c8ec09519d9a55284fd6db2efafb03c8
Simon Lentz <simonemlentz@gmail.com>
2026-01-06 17:12:19 +0100

    Cleanup WAL module

--- lib/goblin/wal.ex
+++ lib/goblin/wal.ex
@@ -2,19 +2,18 @@ defmodule Goblin.WAL do
   @moduledoc false
   use GenServer
 
-  @log_file "wal.goblin"
+  alias Goblin.Manifest
+  alias Goblin.Cleaner
 
-  @typep wal :: module() | {:via, Registry, {module(), module()}}
+  @log_file "wal.goblin"
 
   defstruct [
     :wal,
     :name,
     :ro_name,
     :dir,
-    :manifest,
-    :reader,
-    :task_mod,
-    :task_sup,
+    :manifest_server,
+    :cleaner_server,
     rotations: [],
     cleaning: %{}
   ]
@@ -26,31 +25,29 @@ defmodule Goblin.WAL do
     args = [
       local_name: opts[:local_name] || name,
       db_dir: opts[:db_dir],
-      manifest: opts[:manifest],
-      reader: opts[:reader],
-      task_mod: opts[:task_mod],
-      task_sup: opts[:task_sup]
+      manifest_server: opts[:manifest_server],
+      cleaner_server: opts[:cleaner_server]
     ]
 
     GenServer.start_link(__MODULE__, args, name: name)
   end
 
-  @spec append(wal(), [term()]) :: :ok | {:error, term()}
+  @spec append(Goblin.server(), [term()]) :: :ok | {:error, term()}
   def append(wal, logs) do
     GenServer.call(wal, {:append, logs})
   end
 
-  @spec rotate(wal()) :: {:ok, Goblin.db_file(), Goblin.db_file()}
+  @spec rotate(Goblin.server()) :: {:ok, Goblin.db_file(), Goblin.db_file()}
   def rotate(wal) do
     GenServer.call(wal, :rotate)
   end
 
-  @spec clean(wal(), Goblin.db_file()) :: :ok | {:error, term()}
+  @spec clean(Goblin.server(), Goblin.db_file()) :: :ok | {:error, term()}
   def clean(wal, rotation) do
     GenServer.call(wal, {:clean, rotation})
   end
 
-  @spec get_log_streams(wal()) :: [{Goblin.db_file(), Enumerable.t()}]
+  @spec get_log_streams(Goblin.server()) :: [{Goblin.db_file(), Enumerable.t()}]
   def get_log_streams(wal) do
     GenServer.call(wal, :get_log_streams)
   end
@@ -60,13 +57,14 @@ defmodule Goblin.WAL do
     name = args[:local_name]
     ro_name = Module.concat(name, RO)
     db_dir = args[:db_dir]
-    manifest = args[:manifest]
+    manifest_server = args[:manifest_server]
+    cleaner_server = args[:cleaner_server]
 
     {{_count, file} = wal, rotations} =
-      case Goblin.Manifest.get_version(manifest, [:wal, :wal_rotations]) do
+      case Manifest.snapshot(manifest_server, [:wal, :wal_rotations]) do
         %{wal: nil, wal_rotations: []} ->
           {_, file} = wal = new_wal(db_dir, [])
-          Goblin.Manifest.log_wal(manifest, file)
+          Manifest.log_wal(manifest_server, file)
           {wal, []}
 
         %{wal: wal, wal_rotations: rotations} ->
@@ -82,10 +80,8 @@ defmodule Goblin.WAL do
            dir: db_dir,
            wal: wal,
            rotations: rotations,
-           manifest: manifest,
-           reader: args[:reader],
-           task_mod: args[:task_mod] || Task.Supervisor,
-           task_sup: args[:task_sup]
+           manifest_server: manifest_server,
+           cleaner_server: cleaner_server
          }}
 
       {:error, reason} ->
@@ -117,50 +113,14 @@ defmodule Goblin.WAL do
   end
 
   def handle_call({:clean, rotation}, _from, state) do
-    ref = clean_up(state, rotation)
-    cleaning = Map.put(state.cleaning, ref, rotation)
-    state = %{state | cleaning: cleaning}
-    {:reply, :ok, state}
+    Cleaner.clean(state.cleaner_server, [rotation])
+    rotations = Enum.reject(state.rotations, fn {_, file} -> file == rotation end)
+    {:reply, :ok, %{state | rotations: rotations}}
   end
 
   def handle_call(:get_log_streams, _from, state) do
     {:reply, recover_logs(state), state}
   end
-
-  @impl GenServer
-  def handle_info({ref, {:ok, :cleaned}}, state) do
-    {cleaned, cleaning} = Map.pop(state.cleaning, ref)
-
-    case cleaned do
-      nil ->
-        {:noreply, state}
-
-      rotation ->
-        rotations = Enum.reject(state.rotations, fn {_, file} -> file == rotation end)
-        state = %{state | rotations: rotations, cleaning: cleaning}
-        {:noreply, state}
-    end
-  end
-
-  def handle_info({ref, {:error, _reason} = error}, state) do
-    {clean, _cleaning} = Map.pop(state.cleaning, ref)
-
-    case clean do
-      nil -> {:noreply, state}
-      _ -> {:stop, error, state}
-    end
-  end
-
-  def handle_info({:DOWN, ref, _, _, reason}, state) do
-    {clean, _cleaning} = Map.pop(state.cleaning, ref)
-
-    case clean do
-      nil -> {:noreply, state}
-      _ -> {:stop, {:error, reason}, state}
-    end
-  end
-
-  def handle_info(_msg, state), do: {:noreply, state}
 
   defp rotate_log(name, file) do
     with :ok <- close_log(name) do
@@ -179,24 +139,6 @@ defmodule Goblin.WAL do
   end
 
   defp close_log(name), do: :disk_log.close(name)
-
-  defp clean_up(state, file) do
-    %{
-      reader: reader,
-      task_mod: task_mod,
-      task_sup: task_sup
-    } = state
-
-    %{ref: ref} =
-      task_mod.async(task_sup, fn ->
-        with :ok <- Goblin.Reader.empty?(reader),
-             :ok <- File.rm(file) do
-          {:ok, :cleaned}
-        end
-      end)
-
-    ref
-  end
 
   defp append_and_sync_log(name, logs) do
     with :ok <- :disk_log.log_terms(name, logs) do
@@ -256,8 +198,15 @@ defmodule Goblin.WAL do
       rotations
       |> List.keysort(0)
       |> Enum.reduce_while(0, fn
-        {count, _file}, acc when acc < count -> {:halt, acc}
-        _, acc -> {:cont, acc + 1}
+        {count, _file}, acc when acc < count ->
+          if File.exists?(Path.join(dir, "#{@log_file}.#{acc}")) do
+            {:cont, acc + 1}
+          else
+            {:halt, acc}
+          end
+
+        _, acc ->
+          {:cont, acc + 1}
       end)
 
     {new_count, Path.join(dir, "#{@log_file}.#{new_count}")}
--- test/goblin/wal_test.exs
+++ test/goblin/wal_test.exs
@@ -1,156 +1,62 @@
 defmodule Goblin.WALTest do
-  @moduledoc """
-  Important qualities to test:
-
-  - Non-trivial API:
-    - `append/2`
-    - `rotate/1`
-    - `clean/2`
-    - `get_log_streams/1`
-  - Properties:
-    - Recovers state from Manifest on start
-  """
   use ExUnit.Case, async: true
   use TestHelper
+  use Mimic
   alias Goblin.WAL
 
   @moduletag :tmp_dir
   setup_db()
 
-  describe "API" do
-    test "append/2 appends to log and syncs", c do
-      %{wal: {_, file}} = :sys.get_state(c.wal)
-      %{size: size1} = File.stat!(file)
+  test "can append and read logs", c do
+    assert [{wal, log_stream}] = WAL.get_log_streams(c.wal)
+    assert [] == Enum.to_list(log_stream)
+    %{size: size1} = File.stat!(wal)
 
-      assert :ok == WAL.append(c.wal, [{:put, 0, 0, 0}, {:put, 1, 1, 1}])
+    assert :ok == WAL.append(c.wal, [{:put, 0, 0, 0}])
 
-      %{size: size2} = File.stat!(file)
-      assert size2 > size1
+    assert [{wal, log_stream}] = WAL.get_log_streams(c.wal)
+    assert [{:put, 0, 0, 0}] == Enum.to_list(log_stream)
+    %{size: size2} = File.stat!(wal)
+    assert size2 > size1
+  end
+
+  test "can rotate and clean log files", c do
+    assert :ok == WAL.append(c.wal, [{:put, 0, 0, 0}])
+    assert {:ok, rotation, current} = WAL.rotate(c.wal)
+    assert :ok == WAL.append(c.wal, [{:put, 1, 1, 1}])
+
+    assert [{^rotation, log_stream1}, {^current, log_stream2}] = WAL.get_log_streams(c.wal)
+
+    assert [{:put, 0, 0, 0}] == Enum.to_list(log_stream1)
+    assert [{:put, 1, 1, 1}] == Enum.to_list(log_stream2)
+
+    assert :ok == WAL.clean(c.wal, rotation)
+
+    assert_eventually do
+      assert [{^current, log_stream}] = WAL.get_log_streams(c.wal)
+      assert [{:put, 1, 1, 1}] == Enum.to_list(log_stream)
     end
 
-    test "rotate/1 generates a new WAL", c do
-      no_files = length(File.ls!(c.tmp_dir))
-      WAL.append(c.wal, [{:put, 0, 0, 0}, {:put, 1, 1, 1}])
-
-      assert {:ok, rotation, current} = WAL.rotate(c.wal)
-
-      WAL.append(c.wal, [{:put, 2, 2, 2}])
-
-      assert no_files + 1 == length(File.ls!(c.tmp_dir))
-
-      assert [{^rotation, stream1}, {^current, stream2}] = WAL.get_log_streams(c.wal)
-      assert [{:put, 0, 0, 0}, {:put, 1, 1, 1}] == Enum.to_list(stream1)
-      assert [{:put, 2, 2, 2}] == Enum.to_list(stream2)
-    end
-
-    test "clean/2 removes log file from disk and state", c do
-      {:ok, rotation, _current} = WAL.rotate(c.wal)
-      no_files = length(File.ls!(c.tmp_dir))
-
-      assert %{rotations: [_]} = :sys.get_state(c.wal)
-
-      assert :ok == WAL.clean(c.wal, rotation)
-
-      assert_eventually do
-        assert %{rotations: []} = :sys.get_state(c.wal)
-      end
-
-      assert no_files - 1 == length(File.ls!(c.tmp_dir))
-    end
-
-    test "clean/2 waits until there are no active readers before cleaning", c do
-      parent = self()
-      {:ok, rotation, _current} = WAL.rotate(c.wal)
-
-      reader =
-        spawn(fn ->
-          Goblin.transaction(
-            c.db,
-            fn _tx ->
-              send(parent, :ready)
-
-              receive do
-                :cont -> :ok
-              end
-            end,
-            read_only: true
-          )
-
-          send(parent, :done)
-        end)
-
-      assert_receive :ready
-
-      assert :ok == WAL.clean(c.wal, rotation)
-
-      assert %{rotations: [{_, ^rotation}]} = :sys.get_state(c.wal)
-
-      send(reader, :cont)
-
-      assert_receive :done
-
-      assert_eventually do
-        assert %{rotations: []} = :sys.get_state(c.wal)
-      end
-    end
-
-    test "get_log_streams/1 returns all logs", c do
-      assert [{_current_wal, stream}] = WAL.get_log_streams(c.wal)
-      assert [] == Enum.to_list(stream)
-      WAL.append(c.wal, [{:put, 0, 0, 0}, {:put, 1, 1, 1}])
-      assert [{_current_wal, stream}] = WAL.get_log_streams(c.wal)
-      assert [{:put, 0, 0, 0}, {:put, 1, 1, 1}] == Enum.to_list(stream)
-
-      {:ok, rotation, current} = WAL.rotate(c.wal)
-      Goblin.Manifest.log_rotation(c.manifest, rotation, current)
-
-      assert [
-               {^rotation, stream1},
-               {^current, stream2}
-             ] = WAL.get_log_streams(c.wal)
-
-      assert [{:put, 0, 0, 0}, {:put, 1, 1, 1}] == Enum.to_list(stream1)
-      assert [] == Enum.to_list(stream2)
-
-      WAL.append(c.wal, [{:put, 2, 2, 2}, {:put, 3, 3, 3}])
-
-      assert [
-               {^rotation, stream1},
-               {^current, stream2}
-             ] = WAL.get_log_streams(c.wal)
-
-      assert [{:put, 0, 0, 0}, {:put, 1, 1, 1}] == Enum.to_list(stream1)
-      assert [{:put, 2, 2, 2}, {:put, 3, 3, 3}] == Enum.to_list(stream2)
-
-      stop_db(__MODULE__)
-      %{db: db, wal: wal} = start_db(c.tmp_dir, name: __MODULE__)
-
-      assert [
-               {^rotation, stream1},
-               {^current, stream2}
-             ] = WAL.get_log_streams(wal)
-
-      # wait for Writer to finish recovering...
-      Goblin.is_flushing(db)
-
-      assert [{:put, 0, 0, 0}, {:put, 1, 1, 1}] == Enum.to_list(stream1)
-      assert [{:put, 2, 2, 2}, {:put, 3, 3, 3}] == Enum.to_list(stream2)
+    assert_raise RuntimeError, fn ->
+      Enum.to_list(log_stream1)
     end
   end
 
-  describe "Property" do
-    test "recovers state from Manifest on restart", c do
-      WAL.append(c.wal, [{:put, 0, 0, 0}, {:put, 1, 1, 1}])
-      {:ok, rotation, current} = WAL.rotate(c.wal)
+  test "gets state from Manifest on start", c do
+    assert :ok == WAL.append(c.wal, [{:put, 0, 0, 0}])
+    assert {:ok, rotation, current} = WAL.rotate(c.wal)
+    assert :ok == WAL.append(c.wal, [{:put, 1, 1, 1}])
+    Goblin.Manifest.log_rotation(c.manifest, rotation, current)
 
-      wal_state = :sys.get_state(c.wal)
+    assert [{^rotation, log_stream1}, {^current, log_stream2}] = WAL.get_log_streams(c.wal)
+    assert [{:put, 0, 0, 0}] == Enum.to_list(log_stream1)
+    assert [{:put, 1, 1, 1}] == Enum.to_list(log_stream2)
 
-      Goblin.Manifest.log_rotation(c.manifest, rotation, current)
+    stop_db(__MODULE__)
+    %{wal: wal} = start_db(c.tmp_dir, name: __MODULE__)
 
-      stop_db(__MODULE__)
-      %{wal: wal} = start_db(c.tmp_dir, name: __MODULE__)
-
-      assert ^wal_state = :sys.get_state(wal)
-    end
+    assert [{^rotation, log_stream1}, {^current, log_stream2}] = WAL.get_log_streams(wal)
+    assert [{:put, 0, 0, 0}] == Enum.to_list(log_stream1)
+    assert [{:put, 1, 1, 1}] == Enum.to_list(log_stream2)
   end
 end
