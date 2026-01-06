074197842901d48276b29aa8fa75256a9d71ef5a
Simon Lentz <simonemlentz@gmail.com>
2026-01-06 17:12:33 +0100

    Small changes

--- lib/goblin/pub_sub.ex
+++ lib/goblin/pub_sub.ex
@@ -1,8 +1,6 @@
 defmodule Goblin.PubSub do
   @moduledoc false
 
-  @typep pub_sub :: module() | {:via, Registry, {module(), module()}}
-
   @spec start_link(keyword()) :: {:ok, pid()} | {:error, term()}
   def start_link(args) do
     Registry.start_link(
@@ -21,21 +19,23 @@ defmodule Goblin.PubSub do
     )
   end
 
-  @spec subscribe(pub_sub()) :: {:ok, pid()} | {:error, term()}
+  @spec subscribe(Goblin.server()) :: {:ok, pid()} | {:error, term()}
   def subscribe(pub_sub) do
     Registry.register(pub_sub, pub_sub, [])
   end
 
-  @spec unsubscribe(pub_sub()) :: :ok
+  @spec unsubscribe(Goblin.server()) :: :ok
   def unsubscribe(pub_sub) do
     Registry.unregister(pub_sub, pub_sub)
   end
 
-  @spec publish(pub_sub(), term()) :: :ok
+  @spec publish(Goblin.server(), [
+          {:put, Goblin.db_key(), Goblin.db_value()} | {:remove, Goblin.db_key()}
+        ]) :: :ok
   def publish(pub_sub, writes) do
     Registry.dispatch(pub_sub, pub_sub, fn entries ->
       for {pid, _} <- entries do
-        for write <- writes, do: send(pid, Tuple.delete_at(write, 1))
+        for write <- writes, do: send(pid, write)
       end
     end)
   end
