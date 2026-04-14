d178023505f2e48ea145724d8a5e64b52a6f06ec
Simon Lentz <simonemlentz@gmail.com>
2026-04-14 07:15:13 +0200

    refactor and optimize k_merge_stream

--- lib/goblin/iterator.ex
+++ lib/goblin/iterator.ex
@@ -24,21 +24,23 @@ defmodule Goblin.Iterator do
     Stream.resource(
       fn ->
         init_f.()
-        |> Enum.flat_map(fn iterator ->
+        |> Enum.reduce(:gb_trees.empty(), fn iterator, acc ->
           iterator = Goblin.Iterable.init(iterator)
 
           case iterate(iterator) do
-            :ok -> []
-            {triple, iterator} -> [{iterator, triple}]
+            :ok ->
+              acc
+
+            {{key, seq, _val} = triple, iterator} ->
+              :gb_trees.insert({key, -seq, make_ref()}, {iterator, triple}, acc)
           end
         end)
       end,
       &k_merge(&1, opts),
-      fn cursors ->
-        Enum.each(cursors, fn
-          {nil, _} -> :ok
-          {iterator, _} -> Goblin.Iterable.deinit(iterator)
-          _ -> :ok
+      fn heap ->
+        :gb_trees.values(heap)
+        |> Enum.each(fn {iterator, _} ->
+          Goblin.Iterable.deinit(iterator)
         end)
 
         opts[:after] && opts[:after].()
@@ -46,28 +48,72 @@ defmodule Goblin.Iterator do
     )
   end
 
-  defp k_merge(cursors, opts) do
+  defp k_merge(heap, opts) do
     filter_tombstones? = Keyword.get(opts, :filter_tombstones?, true)
     min = opts[:min]
     max = opts[:max]
 
-    cursors = Enum.sort_by(cursors, fn {_, {key, seq, _}} -> {key, -seq} end)
+    case take_smallest(heap) do
+      :empty ->
+        {:halt, heap}
 
-    case cursors do
-      [] ->
-        {:halt, []}
+      {{key, _, _}, heap} when not is_nil(max) and key > max ->
+        {:halt, heap}
 
-      [{_, {smallest_key, _, _}} | _] when not is_nil(max) and smallest_key > max ->
-        {:halt, cursors}
+      {{key, _, _}, heap} when not is_nil(min) and key < min ->
+        {[], heap}
 
-      [{_, {smallest_key, _, _}} | _] when not is_nil(min) and smallest_key < min ->
-        {[], Enum.flat_map(cursors, &jump(&1, smallest_key))}
+      {{_, _, :"$goblin_tombstone"}, heap} when filter_tombstones? ->
+        {[], heap}
 
-      [{_, {smallest_key, _, :"$goblin_tombstone"}} | _] when filter_tombstones? ->
-        {[], Enum.flat_map(cursors, &jump(&1, smallest_key))}
+      {triple, heap} ->
+        {[triple], heap}
+    end
+  end
 
-      [{_, {smallest_key, _, _} = next} | _] ->
-        {[next], Enum.flat_map(cursors, &jump(&1, smallest_key))}
+  defp take_smallest(heap) do
+    if :gb_trees.is_empty(heap) do
+      :empty
+    else
+      {{key, _, _}, {_iterator, triple}} = :gb_trees.smallest(heap)
+      heap = advance_next_key(heap, key)
+      {triple, heap}
+    end
+  end
+
+  defp advance_next_key(heap, key) do
+    if :gb_trees.is_empty(heap) do
+      heap
+    else
+      case :gb_trees.smallest(heap) do
+        {{k, _, _}, _cursor} when k == key ->
+          {_, value, heap} = :gb_trees.take_smallest(heap)
+          heap = reinsert_advanced(heap, key, value)
+          advance_next_key(heap, key)
+
+        _ ->
+          heap
+      end
+    end
+  end
+
+  defp reinsert_advanced(heap, key, cursor) do
+    case advance_cursor(cursor, key) do
+      nil -> heap
+      {_, {k, s, _}} = cursor -> :gb_trees.insert({k, -s, make_ref()}, cursor, heap)
+    end
+  end
+
+  defp advance_cursor({iterator, {key1, _, _}}, key2) when key1 == key2 do
+    case iterate(iterator) do
+      :ok ->
+        nil
+
+      {{k, _, _} = triple, iterator} when k == key2 ->
+        advance_cursor({iterator, triple}, key2)
+
+      {triple, iterator} ->
+        {iterator, triple}
     end
   end
 
@@ -77,17 +123,4 @@ defmodule Goblin.Iterator do
       {out, iterator} -> {out, iterator}
     end
   end
-
-  defp jump({nil, {key1, _, _}}, key2) when key1 == key2, do: []
-  defp jump({nil, triple}, _), do: [{nil, triple}]
-
-  defp jump({iterator, {key1, _, _}}, key2) when key1 == key2 do
-    case iterate(iterator) do
-      :ok -> []
-      {{key, _, _} = triple, iterator} when key == key1 -> jump({iterator, triple}, key2)
-      {next, iterator} -> [{iterator, next}]
-    end
-  end
-
-  defp jump(cursor, _key), do: [cursor]
 end
