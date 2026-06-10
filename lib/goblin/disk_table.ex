b9f87b8f402f6ed23da108b4b454e2d285e0d66e
Simon Lentz <simonemlentz@gmail.com>
2026-06-10 06:50:42 +0200

    change to variable sized blocks with sparse + dense index

--- lib/goblin/disk_table.ex
+++ lib/goblin/disk_table.ex
@@ -6,7 +6,7 @@ defmodule Goblin.DiskTable do
     FileIO
   }
 
-  @disk_table_block_size 1024
+  @index_interval 4096
 
   defstruct [
     :id,
@@ -14,8 +14,8 @@ defmodule Goblin.DiskTable do
     :bloom_filter,
     :key_range,
     :seq_range,
-    size: 0,
-    no_blocks: 0
+    index: [],
+    size: 0
   ]
 
   @type t :: %__MODULE__{
@@ -24,8 +24,8 @@ defmodule Goblin.DiskTable do
           bloom_filter: BloomFilter.t(),
           key_range: {term(), term()},
           seq_range: {non_neg_integer(), non_neg_integer()},
-          size: non_neg_integer(),
-          no_blocks: non_neg_integer()
+          index: list(),
+          size: non_neg_integer()
         }
 
   @spec build(Enumerable.t({term(), non_neg_integer(), term()}), keyword()) :: list(t())
@@ -38,44 +38,33 @@ defmodule Goblin.DiskTable do
 
     stream
     |> Stream.transform(
-      fn -> {nil, 0, nil} end,
+      fn -> {nil, 0, nil, []} end,
       fn
         _, {:halt, file} ->
           FileIO.close(file)
           {:halt, nil}
 
-        triple, {file, size, acc} ->
-          file = file || FileIO.open!(filer.(), write?: true, block_size: @disk_table_block_size)
+        triple, {file, boundary, acc, offset_index} ->
+          file = file || FileIO.open!(filer.(), write?: true)
           acc = acc || %{dt | id: file.path}
 
-          case FileIO.append(file, triple, compress?: compress?) do
-            {:ok, triple_size} ->
-              acc = add_to_table(acc, triple, triple_size)
-              size = size + triple_size
-
-              if size >= max_size do
-                with {:ok, _} <- FileIO.append(file, acc, compress?: compress?),
-                     :ok <- FileIO.sync(file),
-                     :ok <- FileIO.close(file) do
-                  {[{:ok, acc}], {nil, 0, nil}}
-                else
-                  error -> {[error], {:halt, file}}
-                end
-              else
-                {[], {file, size, acc}}
-              end
-
-            error ->
-              {[error], {:halt, file}}
+          with {:ok, acc, offset_index} <-
+                 append_data(file, acc, offset_index, triple, compress?),
+               {:ok, acc, offset_index, boundary} <-
+                 maybe_append_index(file, acc, offset_index, boundary, compress?),
+               {:ok, acc, wrapped?} <-
+                 maybe_append_footer(file, acc, offset_index, max_size, compress?) do
+            if wrapped?,
+              do: {[{:ok, acc}], {nil, 0, nil, []}},
+              else: {[], {file, boundary, acc, offset_index}}
+          else
+            error -> {[error], {:halt, file}}
           end
       end,
       fn
-        {%FileIO{} = file, _, acc} ->
-          with {:ok, _} <- FileIO.append(file, acc, compress?: compress?),
-               :ok <- FileIO.sync(file),
-               :ok <- FileIO.close(file) do
-            {[{:ok, acc}], nil}
-          else
+        {%FileIO{} = file, _, acc, offset_index} ->
+          case maybe_append_footer(file, acc, offset_index, 0, compress?) do
+            {:ok, acc, _} -> {[{:ok, acc}], nil}
             error -> {[error], nil}
           end
 
@@ -83,7 +72,7 @@ defmodule Goblin.DiskTable do
           {[], nil}
       end,
       fn
-        {%FileIO{} = file, _, _} -> FileIO.close(file)
+        {%FileIO{} = file, _, _, _} -> FileIO.close(file)
         _ -> :ok
       end
     )
@@ -95,11 +84,11 @@ defmodule Goblin.DiskTable do
 
   @spec from_file(Path.t()) :: {:ok, t()} | {:error, term()}
   def from_file(path) do
-    io = FileIO.open!(path, block_size: @disk_table_block_size)
+    io = FileIO.open!(path)
 
     try do
-      case FileIO.pread(io, :eof) do
-        {:ok, %__MODULE__{} = table} -> {:ok, table}
+      case FileIO.read_footer(io) do
+        {:ok, %__MODULE__{} = dt} -> {:ok, dt}
         {:ok, _} -> {:error, :invalid_disk_table}
         error -> error
       end
@@ -116,35 +105,18 @@ defmodule Goblin.DiskTable do
   @spec search(t(), list(term()), non_neg_integer()) ::
           Enumerable.t({term(), non_neg_integer(), term()})
   def search(dt, keys, seq) do
-    Stream.resource(
-      fn ->
-        io = FileIO.open!(dt.id, block_size: @disk_table_block_size)
-        low = 1
-        high = dt.no_blocks
-        {io, {low, high}, keys}
+    Stream.transform(
+      keys,
+      fn -> FileIO.open!(dt.id) end,
+      fn key, io ->
+        case lookup(io, dt.index, key, seq) do
+          {:ok, nil} -> {[], io}
+          {:ok, triple} -> {[triple], io}
+          :eof -> {:halt, io}
+          error -> raise "search failed with error: #{inspect(error)}"
+        end
       end,
-      fn
-        {io, _, []} ->
-          FileIO.close(io)
-          {:halt, nil}
-
-        {io, {low, high}, [next | keys]} ->
-          case binary_search(io, next, low, high, seq) do
-            {:ok, new_low, triple} ->
-              {[triple], {io, {new_low, high}, keys}}
-
-            {:ok, :not_found} ->
-              {[], {io, {low, high}, keys}}
-
-            error ->
-              FileIO.close(io)
-              raise "search failed with error: #{inspect(error)}"
-          end
-      end,
-      fn
-        {io, _, _} -> FileIO.close(io)
-        _ -> :ok
-      end
+      fn io -> FileIO.close(io) end
     )
   end
 
@@ -155,11 +127,14 @@ defmodule Goblin.DiskTable do
 
     if within_bounds?(dt, min, max) do
       Stream.resource(
-        fn -> FileIO.open!(dt.id, block_size: @disk_table_block_size) end,
+        fn -> FileIO.open!(dt.id) end,
         fn io ->
-          case FileIO.read(io) do
+          case FileIO.read(io, verify_crc?: false) do
             {:ok, {_, s, _} = triple} when s < seq ->
               {[triple], io}
+
+            {:ok, %__MODULE__{}} ->
+              {:halt, io}
 
             {:ok, _} ->
               {[], io}
@@ -179,7 +154,56 @@ defmodule Goblin.DiskTable do
     end
   end
 
-  defp add_to_table(dt, triple, size) do
+  defp maybe_append_index(file, %{size: size} = dt, offset_index, boundary, compress?)
+       when size - boundary >= @index_interval do
+    with {:ok, dt, offset_index} <- append_index(file, dt, offset_index, compress?) do
+      {:ok, dt, offset_index, dt.size}
+    end
+  end
+
+  defp maybe_append_index(_, dt, offset_index, boundary, _), do: {:ok, dt, offset_index, boundary}
+
+  defp maybe_append_footer(_, %{size: size} = dt, _, max_size, _) when size < max_size,
+    do: {:ok, dt, false}
+
+  defp maybe_append_footer(file, dt, offset_index, _max_size, compress?) do
+    with {:ok, dt, _} <- append_index(file, dt, offset_index, compress?),
+         dt = finalize_table(dt),
+         :ok <- append_footer(file, dt, compress?) do
+      {:ok, dt, true}
+    end
+  end
+
+  defp append_data(file, dt, offset_index, triple, compress?) do
+    {key, seq, _} = triple
+
+    with {:ok, size} <- FileIO.append(file, triple, compress?: compress?) do
+      offset_index = [{key, seq, dt.size} | offset_index]
+      {:ok, update_table(dt, triple, size), offset_index}
+    end
+  end
+
+  defp append_index(_, dt, [], _), do: {:ok, dt, []}
+
+  defp append_index(file, dt, offset_index, compress?) do
+    [{first_key, _, _} | _] = offset_index = offset_index |> Enum.reverse()
+
+    with {:ok, inc_size} <- FileIO.append(file, offset_index, compress?: compress?) do
+      index = [{first_key, dt.size} | dt.index]
+      size = dt.size + inc_size
+      dt = %{dt | size: size, index: index}
+      {:ok, dt, []}
+    end
+  end
+
+  defp append_footer(file, dt, compress?) do
+    with {:ok, _} <- FileIO.append(file, dt, compress?: compress?, footer?: true),
+         :ok <- FileIO.sync(file) do
+      FileIO.close(file)
+    end
+  end
+
+  defp update_table(dt, triple, size) do
     {key, seq, _val} = triple
 
     key_range =
@@ -195,81 +219,72 @@ defmodule Goblin.DiskTable do
       end
 
     bloom_filter = BloomFilter.put(dt.bloom_filter, key)
-    no_blocks = dt.no_blocks + div(size, @disk_table_block_size)
 
     %{
       dt
       | key_range: key_range,
         seq_range: seq_range,
         bloom_filter: bloom_filter,
-        no_blocks: no_blocks,
         size: dt.size + size
     }
   end
 
-  defp binary_search(_io, _key, low, high, _seq) when high < low do
-    {:ok, :not_found}
-  end
+  defp finalize_table(dt),
+    do: %{dt | index: Enum.reverse(dt.index) |> List.to_tuple()}
 
-  defp binary_search(io, key, low, high, seq) do
-    mid = div(low + high, 2)
-    pos = (mid - 1) * @disk_table_block_size
+  defp lookup(io, index, key, seq) do
+    {_, start} = elem(index, 0)
+    offset = find_offset(index, key, 0, tuple_size(index) - 1, start)
 
-    case FileIO.pread(io, pos) do
-      {:ok, {k, s, _v} = triple} when k == key and s < seq ->
-        check_left_neighbour(io, mid - 1, triple, seq)
-
-      {:ok, {k, s, _v} = triple} when k == key and s >= seq ->
-        check_right_neighbour(io, mid + 1, triple, seq)
-
-      {:ok, {k, _s, _v}} when key < k ->
-        binary_search(io, key, low, mid - 1, seq)
-
-      {:ok, {k, _s, _v}} when key > k ->
-        binary_search(io, key, mid + 1, high, seq)
-
-      error ->
-        error
+    # with {:ok, _} <- FileIO.set_position(io, offset),
+    with {:ok, offset_index} <- FileIO.read(io, verify_crc?: false, offset: offset),
+         offset when is_integer(offset) <- find_lower_bound(offset_index, key, seq),
+         {:ok, _} <- FileIO.set_position(io, offset) do
+      scan(io, key, seq)
+    else
+      :not_found -> {:ok, nil}
+      error -> error
     end
   end
 
-  defp check_left_neighbour(_io, block_no, triple, _seq) when block_no <= 0 do
-    {:ok, block_no + 1, triple}
+  defp find_offset(_index, _target, lo, hi, offset) when lo > hi, do: offset
+
+  defp find_offset(index, target, lo, hi, offset) do
+    mid = div(lo + hi, 2)
+    {key, new_offset} = elem(index, mid)
+
+    if key < target,
+      do: find_offset(index, target, mid + 1, hi, new_offset),
+      else: find_offset(index, target, lo, mid - 1, offset)
   end
 
-  defp check_left_neighbour(io, block_no, {key, _, _} = triple, seq) do
-    pos = (block_no - 1) * @disk_table_block_size
+  defp find_lower_bound(index, target_key, target_seq, best \\ 0)
+  defp find_lower_bound([], _, _, best), do: best
 
-    case FileIO.pread(io, pos) do
-      {:ok, {k, s, _v} = neighbouring_triple} when k == key and s < seq ->
-        check_left_neighbour(io, block_no - 1, neighbouring_triple, seq)
+  defp find_lower_bound([{key, seq, offset} | rest], target_key, target_seq, _best) do
+    cond do
+      key < target_key ->
+        find_lower_bound(rest, target_key, target_seq, offset)
 
-      {:ok, _neighbouring_triple} ->
-        {:ok, block_no + 1, triple}
+      key == target_key and seq >= target_seq ->
+        find_lower_bound(rest, target_key, target_seq, offset)
 
-      error ->
-        error
+      key == target_key ->
+        offset
+
+      true ->
+        :not_found
     end
   end
 
-  defp check_right_neighbour(io, block_no, {key, _, _}, seq) do
-    pos = (block_no - 1) * @disk_table_block_size
-
-    case FileIO.pread(io, pos) do
-      {:ok, {k, s, _v} = neighbouring_triple} when k == key and s >= seq ->
-        check_right_neighbour(io, block_no + 1, neighbouring_triple, seq)
-
-      {:ok, {k, _s, _v} = neighbouring_triple} when k == key ->
-        {:ok, block_no + 1, neighbouring_triple}
-
-      {:ok, _neighbouring_triple} ->
-        {:ok, :not_found}
-
-      :eof ->
-        {:ok, :not_found}
-
-      error ->
-        error
+  defp scan(io, key, seq) do
+    case FileIO.read(io, verify_crc?: false) do
+      {:ok, {k, s, _}} when k == key and s >= seq -> scan(io, key, seq)
+      {:ok, {k, _, _}} when k > key -> {:ok, nil}
+      {:ok, {k, _, _} = triple} when k == key -> {:ok, triple}
+      {:ok, %__MODULE__{}} -> {:ok, nil}
+      {:ok, _} -> scan(io, key, seq)
+      error -> error
     end
   end
 
--- lib/goblin/file_io.ex
+++ lib/goblin/file_io.ex
@@ -2,8 +2,7 @@ defmodule Goblin.FileIO do
   @moduledoc false
 
   @magic "GOBLIN00"
-  @header_size byte_size(<<@magic::binary, 0::integer-32, 0::integer-32, 0::integer-32>>)
-  @default_block_size 512
+  @header_size byte_size(<<@magic::binary, 0::integer-64, 0::integer-32>>)
 
   @default_modes [
     :raw,
@@ -14,14 +13,12 @@ defmodule Goblin.FileIO do
 
   defstruct [
     :path,
-    :iodev,
-    :block_size
+    :iodev
   ]
 
   @type t :: %__MODULE__{
           path: Path.t(),
-          iodev: :file.io_device(),
-          block_size: non_neg_integer()
+          iodev: :file.io_device()
         }
 
   @spec open!(Path.t(), keyword()) :: t()
@@ -44,53 +41,53 @@ defmodule Goblin.FileIO do
       {:ok,
        %__MODULE__{
          path: path,
-         iodev: iodev,
-         block_size: opts[:block_size] || @default_block_size
+         iodev: iodev
        }}
     end
   end
 
   @spec append(t(), term(), keyword()) :: {:ok, non_neg_integer()} | {:error, term()}
   def append(file, term, opts \\ []) do
-    iolist = encode_to_iolist(term, file.block_size, opts[:compress?] || false)
+    compress? = opts[:compress?] || false
+    footer? = opts[:footer?] || false
+    iolist = encode_to_iolist(term, compress?, footer?)
 
     with :ok <- :file.write(file.iodev, iolist) do
       {:ok, :erlang.iolist_size(iolist)}
     end
   end
 
-  @spec read(t()) :: {:ok, term()} | {:error, term()} | :eof
-  def read(file) do
-    with {:ok, header} <- :file.read(file.iodev, @header_size),
-         {:ok, no_blocks, block_size, crc} <- decode_header(header),
-         {:ok, payload} <- :file.read(file.iodev, no_blocks * block_size - @header_size),
-         :ok <- validate_size(byte_size(payload), no_blocks * block_size - @header_size),
-         :ok <- validate_crc(payload, crc) do
-      {:ok, :erlang.binary_to_term(payload)}
+  @spec read(t(), keyword()) :: {:ok, term()} | {:error, term()} | :eof
+  def read(file, opts \\ []) do
+    reader =
+      cond do
+        opts[:offset] ->
+          fn offset, size -> :file.pread(file.iodev, opts[:offset] + offset, size) end
+
+        true ->
+          fn _, size -> :file.read(file.iodev, size) end
+      end
+
+    with {:ok, header} <- reader.(0, @header_size),
+         {:ok, size, crc} <- decode_header(header),
+         {:ok, payload} <- reader.(@header_size, size),
+         :ok <- validate_size(byte_size(payload), size),
+         :ok <- validate_crc(payload, crc, Keyword.get(opts, :verify_crc?, true)) do
+      decode_payload(payload)
     end
   end
 
-  @spec pread(t(), non_neg_integer() | :eof) :: {:ok, term()} | {:error, term()} | :eof
-  def pread(_file, pos) when pos < 0, do: {:error, :invalid_position}
+  @spec read_footer(t()) :: {:ok, term()} | {:error, term()} | :eof
+  def read_footer(file) do
+    size = :filelib.file_size(file.path)
 
-  def pread(file, :eof) do
-    case :filelib.file_size(file.path) do
-      0 -> {:error, :empty}
-      size -> pread(file, (div(size, file.block_size) - 1) * file.block_size)
-    end
-  end
-
-  def pread(file, pos) do
-    with {:ok, header} <- :file.pread(file.iodev, pos, @header_size),
-         {:ok, no_blocks, block_size, crc} <- decode_header(header),
+    with {:ok, header} <- :file.pread(file.iodev, size - @header_size, @header_size),
+         {:ok, payload_size, crc} <- decode_header(header),
          {:ok, payload} <-
-           :file.pread(file.iodev, pos + @header_size, no_blocks * block_size - @header_size),
-         :ok <- validate_size(byte_size(payload), no_blocks * block_size - @header_size),
-         :ok <- validate_crc(payload, crc) do
-      {:ok, :erlang.binary_to_term(payload)}
-    else
-      {:error, :invalid_header} -> pread(file, (div(pos, file.block_size) - 1) * file.block_size)
-      error -> error
+           :file.pread(file.iodev, size - (@header_size + payload_size), payload_size),
+         :ok <- validate_size(byte_size(payload), payload_size),
+         :ok <- validate_crc(payload, crc, true) do
+      decode_payload(payload)
     end
   end
 
@@ -141,10 +138,13 @@ defmodule Goblin.FileIO do
 
   @spec truncate(t(), non_neg_integer()) :: :ok | {:error, term()}
   def truncate(file, pos) do
-    with {:ok, _} <- :file.position(file.iodev, pos) do
+    with {:ok, _} <- set_position(file, pos) do
       :file.truncate(file.iodev)
     end
   end
+
+  @spec set_position(t(), non_neg_integer()) :: {:ok, non_neg_integer()} | {:error, term()}
+  def set_position(file, pos), do: :file.position(file.iodev, pos)
 
   @spec rename(Path.t(), Path.t()) :: :ok | {:error, term()}
   def rename(from, to), do: File.rename(from, to)
@@ -161,41 +161,44 @@ defmodule Goblin.FileIO do
   @spec size_of(Path.t()) :: non_neg_integer()
   def size_of(path), do: :filelib.file_size(path)
 
-  defp encode_to_iolist(terms, block_size, compress?) do
+  defp encode_to_iolist(terms, compress?, footer?) do
     opts = if compress?, do: [:compressed], else: []
     payload = :erlang.term_to_iovec(terms, opts)
-    payload_size = @header_size + :erlang.iolist_size(payload)
-    no_blocks = div(payload_size + block_size - 1, block_size)
-    padding = no_blocks * block_size - payload_size
+    payload_size = :erlang.iolist_size(payload)
 
-    data = [
-      payload,
-      <<0::size(padding)-unit(8)>>
-    ]
-
-    [
+    header = [
       <<@magic::binary>>,
-      <<no_blocks::integer-32>>,
-      <<block_size::integer-32>>,
-      <<:erlang.crc32(data)::integer-32>>
-      | data
+      <<payload_size::integer-64>>,
+      <<:erlang.crc32(payload)::integer-32>>
     ]
+
+    case footer? do
+      true -> [header, payload, header]
+      _ -> [header, payload]
+    end
+  end
+
+  defp decode_payload(payload) do
+    {:ok, :erlang.binary_to_term(payload)}
+  rescue
+    ArgumentError -> {:error, :invalid_term}
   end
 
   defp decode_header(<<
          @magic::binary,
-         no_blocks::integer-32,
-         block_size::integer-32,
+         payload_size::integer-64,
          crc::integer-32
        >>),
-       do: {:ok, no_blocks, block_size, crc}
+       do: {:ok, payload_size, crc}
 
   defp decode_header(_), do: {:error, :invalid_header}
 
   defp validate_size(size, size), do: :ok
   defp validate_size(_, _), do: {:error, :invalid_size}
 
-  defp validate_crc(payload, crc) do
+  defp validate_crc(_payload, _crc, false), do: :ok
+
+  defp validate_crc(payload, crc, _) do
     case :erlang.crc32(payload) == crc do
       true -> :ok
       false -> {:error, :invalid_crc}
