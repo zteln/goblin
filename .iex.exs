b7675856b066255359c91f8b8604b064efece80e
Simon Lentz <simonemlentz@gmail.com>
2026-06-16 20:22:41 +0200

    update helper .iex.exs for dev

--- .iex.exs
+++ .iex.exs
@@ -1,4 +1,4 @@
-trigger_flush =
+{db, trigger_flush} =
   case System.get_env("GOBLIN_TEST_DIR") do
     nil ->
       & &1
@@ -6,14 +6,14 @@ trigger_flush =
     data_dir ->
       {:ok, db} = Goblin.start_link(data_dir: data_dir)
 
-      fn version ->
-        Stream.iterate(1, &(&1 + 1))
-        |> Stream.chunk_every(10_000)
-        |> Enum.take_while(fn chunk ->
-          chunk = Enum.map(chunk, fn x -> {x, "v#{version}-#{x}"} end)
-          Goblin.put_multi(db, chunk)
-          not Goblin.flushing?(db)
-        end)
-      end
+      {db,
+       fn version ->
+         Stream.iterate(1, &(&1 + 1))
+         |> Stream.chunk_every(10_000)
+         |> Enum.take_while(fn chunk ->
+           chunk = Enum.map(chunk, fn x -> {x, "v#{version}-#{x}"} end)
+           Goblin.put_multi(db, chunk)
+           not Goblin.flushing?(db)
+         end)
+       end}
   end
-
