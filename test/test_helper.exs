11a008e8e9de1583327ef4cc6110c4046fed475f
Simon Lentz <simonemlentz@gmail.com>
2026-07-12 14:45:17 +0200

    remove unnecessary test helper

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,15 +1,1 @@
-defmodule Goblin.TestHelper do
-  @moduledoc false
-
-  def uniq_by_value(list, f \\ & &1) do
-    Enum.reduce(list, [], fn item, acc ->
-      case Enum.any?(acc, &(f.(&1) == f.(item))) do
-        true -> acc
-        false -> [item | acc]
-      end
-    end)
-    |> Enum.reverse()
-  end
-end
-
 ExUnit.start(exclude: :property_tests)
